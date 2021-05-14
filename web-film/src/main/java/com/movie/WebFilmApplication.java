46a3bf1c418c2fc15b25f97385f39b9cb6d73092
Le Cao Trieu <lecaotrieu@gmail.com>
2021-05-14 12:19:30 +0700

    commit 12/5/2021

--- web-film/src/main/java/com/movie/WebFilmApplication.java
+++ web-film/src/main/java/com/movie/WebFilmApplication.java
@@ -3,9 +3,10 @@ package com.movie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebFilmApplication {
+public class WebFilmApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(WebFilmApplication.class, args);
     }
